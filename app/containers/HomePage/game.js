01efaa3ede06542c13a5a3b7c0d33f4ace8ec4d3
ccooool <carterlack@gmail.com>
2019-09-14 11:17:40 -0500

    Trying to add game that is not working

---
+++ app/containers/HomePage/game.js
@@ -0,0 +1,25 @@
+import React, { Component, PropTypes } from 'react';
+import './game.css'
+import './script.js'
+
+
+class Game extends Component {
+    render() {
+
+        <div>
+
+            <div id="container">
+                <canvas ref="canvas" height="900" width="1600"></canvas>
+            </div>
+        </div>
+    }
+
+    componentDidMount() {
+        $(function() {
+
+            <script  src="./script.js"></script>
+        })
+    }
+}
+
+export default Game
--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -5,14 +5,49 @@
  *
  */
 
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
+import Game from './game.js'
 
-export default function HomePage() {
-  return (
-    <h1>
-      <FormattedMessage {...messages.header} />
-    </h1>
-  );
+const getUser = () => {
+  return this.state.user
 }
+
+const startGame = () => {
+  console.log("i was clicked!")
+  
+}
+
+class HomePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: "Carter Lack",
+      game_name: "ucn"
+    };
+  }
+
+  render() {
+
+
+
+    return (
+      <div>
+
+  
+          <h1>
+            Hello World I am {this.state.user}
+          {/* <FormattedMessage {...messages.header} /> */}
+          </h1>
+
+          <Game></Game>
+          
+          {/* <button type="button" onClick={startGame()}>start game</button> */}
+
+      </div>
+    )
+  }
+}
+
+export default HomePage
